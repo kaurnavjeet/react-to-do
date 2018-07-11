a5ba125c39b80f0553e177e2a330bb2193876bb8
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-07-11 01:40:03 -0700

    Assignment Events: completed

--- src/App.js
+++ src/App.js
@@ -39,12 +39,20 @@ class App extends Component {
     })
   }
 
+  deleteToDo(index) {
+    this.setState((currentState) => {
+      return {
+        todos: currentState.todos.filter((todo) => currentState.todos.indexOf(todo) !== index)
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map((todo, index) =>
-            <ToDo key={ index } description={todo.description} isCompleted={todo.isCompleted} toggleComplete={ () => this.toggleComplete(index) }/>
+            <ToDo key={ index } description={todo.description} isCompleted={todo.isCompleted} toggleComplete={ () => this.toggleComplete(index) } delete={ () => this.deleteToDo(index) }/>
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
